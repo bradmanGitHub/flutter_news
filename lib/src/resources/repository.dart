e890af8092125bab787b394a21bca03aa590ae0a
mongkonchai buayai <mongkonchai_kk@hotmail.com>
2018-10-30 15:37:20 +0700

    kk

--- lib/src/resources/repository.dart
+++ lib/src/resources/repository.dart
@@ -6,4 +6,19 @@ import '../model/item_model.dart';
 class Repository {
   NewsDbProvider dbProvider = NewsDbProvider();
   NewsAPIProvider apiProvider = NewsAPIProvider();
+
+  fetchTopIds() {
+    return apiProvider.fetchTopIds();
+  }
+
+  fetchItems(int id) async {
+    var item = await dbProvider.fetchItem(id);
+    if (item != null) {
+      return item;
+    }
+
+    item = await apiProvider.fetchItem(id);
+    await dbProvider.addItem(item);
+    return item;
+  }
 }
