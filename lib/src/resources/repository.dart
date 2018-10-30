99c5fb632a952e532a5eefab0c3212cb7c1cd929
mongkonchai buayai <mongkonchai_kk@hotmail.com>
2018-10-30 15:28:09 +0700

    kk

---
+++ lib/src/resources/repository.dart
@@ -0,0 +1,6 @@
+import 'dart:async';
+import 'news_api_provider.dart';
+import 'news_db_provider.dart';
+import '../model/item_model.dart';
+
+class Repository {}
--- test/src/resources/news_api_provider_test.dart
+++ test/src/resources/news_api_provider_test.dart
@@ -5,8 +5,24 @@ import 'package:http/http.dart';
 import 'package:http/testing.dart';
 
 void main() {
-  test('FetchTopIds returns a list of ids', () {
+  test('FetchTopIds returns a list of ids', () async {
     final newsApi = NewsAPIProvider();
-    final ids = newsApi.fetchTopIds();
+    newsApi.client = MockClient((request) async {
+      return Response(json.encode([1, 2, 3, 4]), 200);
+    });
+
+    final ids = await newsApi.fetchTopIds();
+    expect(ids, [1, 2, 3, 4]);
+  });
+
+  test('FetchItem return a item model', () async {
+    final newsApi = NewsAPIProvider();
+    newsApi.client = MockClient((request) async {
+      final jsonMap = {'id': 123};
+      return Response(json.encode(jsonMap), 200);
+    });
+
+    final item = await newsApi.fetchItem(999);
+    expect(item.id, 123);
   });
 }
